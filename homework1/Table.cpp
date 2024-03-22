9f94bfe7297f2b28cea8b76b7bff304db2effff1
monica pencheva <monikapeneva02@gmail.com>
2024-03-22 16:28:35 +0200

    separete Table

--- homework1/Table.cpp
+++ homework1/Table.cpp
@@ -1,2 +1,87 @@
 #include "Table.h"
 
+void Table::findMaxCountColl()
+{
+    for(int i = 0; i < 10; i++)
+    {
+        if(rows[i].getCountCol() > maxCountColl)
+            maxCountColl = rows[i].getCountCol();
+    }
+}
+void Table::findMaxSpacesForEachColl()
+{
+    for(int i = 0; i < 10; i++)//for the coll
+    {
+        for(int j = 0; j < maxCountColl; j++) // for the rows
+        {
+            if( maxSpacesForEachColl[i] < rows[j].getFieldSizeAtCol(i))
+                maxSpacesForEachColl[i] = rows[j].getFieldSizeAtCol(i);
+        }
+    }
+}
+
+
+Table::Table(){}
+
+//remove later
+void Table::read()
+{
+    findMaxCountColl();
+    findMaxSpacesForEachColl();
+}
+
+
+void Table::addRow()
+{
+    countRows++;
+}
+
+void Table::addSpecialField(const char* str)
+{
+    rows[countRows].addSpecialField(str);
+}
+
+void Table::addField(const char* str)
+{
+    rows[countRows].addField(str);
+}
+
+//????? zashto + 1 za printa bachka
+void Table::print() const{
+    for (int i = 0; i < countRows + 1; i++)// rows
+    {
+        for (int j = 0; j < maxCountColl; j++)//coll
+        {
+            if(strcmp(rows[i].getFieldAtCol(j),"") != 0){
+                rows[i].printField(j);
+            }// print field
+
+            int neededSpaces = maxSpacesForEachColl[j] - rows[i].getFieldSizeAtCol(j);
+
+            for(int s = 0; s < neededSpaces; s++) {
+                cout << " ";
+            }
+
+            cout<<"|";
+        }
+        cout << std::endl;
+    }
+}
+
+void Table::implementTag(char tag, const char* str)
+{
+    switch (tag){
+        case 'r' :addRow(); break;
+        case 'h' : addSpecialField(str); break;
+        case 'd' : addField(str); break;
+    }
+}
+
+//tester
+void Table::printSpacesCol()
+{
+    for(int i = 0; i < maxCountColl; i++)
+    {
+        cout<<"col:"<<i<<" maxSpaces:"<<maxSpacesForEachColl[i] <<"\n";
+    }
+}
--- homework1/Table.h
+++ homework1/Table.h
@@ -1,1 +1,37 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include "Row.h"
+#pragma warning (disable:4996);
 
+class Table{
+    Row rows[10];
+    size_t countRows = -1;
+
+    int maxSpacesForEachColl[10]{0};
+    int maxCountColl = 0;
+
+    void findMaxCountColl();
+    void findMaxSpacesForEachColl();
+
+public:
+
+    Table();
+
+    //remove later
+    void read();
+
+    void addRow();
+
+    void addSpecialField(const char* str);
+
+    void addField(const char* str);
+
+    //????? zashto + 1 za printa bachka
+    void print() const;
+
+    void implementTag(char tag, const char* str);
+
+    //tester
+    void printSpacesCol();
+};
