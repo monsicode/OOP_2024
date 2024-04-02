12dd9d1cc1da0d5b192c97d2cf44be1f5760bf6b
monica pencheva <monikapeneva02@gmail.com>
2024-04-02 18:21:37 +0300

    consolePrinter

---
+++ prepExam1/consolePrinter/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+using myfunc = bool (*)(char ch);
+
+class ConsolePrinter{
+
+    //bool (*censore)(char ch);
+
+    myfunc censore;
+
+public:
+
+    ConsolePrinter():censore([](char ch){return false;}) {}
+    ConsolePrinter(myfunc func):censore(func){}
+
+    void print(const char* str) const
+    {
+        while(*str)
+        {
+            if(censore(*str))
+                cout<<"*";
+            else
+                cout<<*str;
+            str++;
+        }
+    }
+
+};
+
+class stringWrapper{
+
+    const char* str;
+    const ConsolePrinter& printer;
+
+public:
+
+  stringWrapper(const char* str, const ConsolePrinter& printer): str(str), printer(printer){}
+
+  void print() const
+  {
+       printer.print(str);
+  }
+
+
+};
+
+bool noA(char ch)
+{
+    return ch == 'a';
+}
+
+int  main(){
+
+    ConsolePrinter pr(noA);
+    ConsolePrinter pr1([](char ch){return ch =='l';});
+
+    stringWrapper str({"hellao"},pr1);
+
+    str.print();
+
+}
