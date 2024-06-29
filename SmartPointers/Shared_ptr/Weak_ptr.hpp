d52049069848930b7c4254a3609cca2c10694eab
monica pencheva <monikapeneva02@gmail.com>
2024-06-29 19:55:43 +0300

    in process of weakPtr

--- SmartPointers/Shared_ptr/Weak_ptr.hpp
+++ SmartPointers/Shared_ptr/Weak_ptr.hpp
@@ -1,14 +1,29 @@
-//
-// Created by Monika on 6/29/2024.
-//
+#pragma once
+#include "Shared_ptr.hpp"
 
-#ifndef SHARED_PTR_WEAK_PTR_HPP
-#define SHARED_PTR_WEAK_PTR_HPP
+template <typename T>
+class Weak_ptr{
+    T* data;
+    Counter* counter;
 
+    void copyFrom(const Weak_ptr<T>& other);
+    void free();
+    void moveFrom(Weak_ptr<T>&& other);
 
-class Weak_ptr {
+public:
+    Weak_ptr();
 
+    //!!!
+    Weak_ptr(Shared_ptr<T>& ptr);
+
+    Weak_ptr(const Weak_ptr<T>& other);
+    Weak_ptr& operator=(const Weak_ptr<T>& other);
+
+    Weak_ptr(Weak_ptr<T>&& other);
+    Weak_ptr& operator=(Weak_ptr<T>&& other);
+    ~Weak_ptr();
+
+    Shared_ptr<T> lock() const;
+
+    bool expired() const;
 };
-
-
-#endif //SHARED_PTR_WEAK_PTR_HPP
--- SmartPointers/Shared_ptr/baba.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Monika on 6/29/2024.
-//
-
-#include "baba.hpp"
