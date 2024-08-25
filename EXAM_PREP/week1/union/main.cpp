8c9e862e18a9695d20b78d80246386208040a319
monica pencheva <monikapeneva02@gmail.com>
2024-08-25 13:46:43 +0300

    little endian and other

--- EXAM_PREP/week1/union/main.cpp
+++ EXAM_PREP/week1/union/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+using namespace std;
+
+bool isLittleEndian(){
+
+    union test{
+        unsigned char arr[4];
+        uint32_t n = 1;
+    }myTest;
+
+    return myTest.arr[0];
+}
+
+union print{
+    uint32_t n;
+    unsigned char arr[4];
+    short sh;
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    print obj;
+
+    obj.n = 12388; // normal decimal
+    // 64 30 00 00 in hex little endian
+    // d   0 .. .. in ascii
+    cout<<hex<<obj.n;
+
+
+
 }
